0eace725172d90ca1851d710caa66cf9fd09a115
triskelion16 <triskelion.work@gmail.com>
2020-02-29 23:53:33 +0100

    Initial project

---
+++ src/test/java/pl/triskelion/countryinfo/CountryInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.triskelion.countryinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
